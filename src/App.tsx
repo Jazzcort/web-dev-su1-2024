2c134591d88dcd0acffe0ee3644a442478c04e84
jazzcort <jason101011113@gmail.com>
2024-05-07 12:09:31 -0400

    first commit main

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Labs from './Labs';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Web Dev</h1>
+      <Labs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,7 @@
+export default function Lab1() {
+    return (
+        <div>
+            <h2>Lab1</h2>
+        </div>
+    )
+}
