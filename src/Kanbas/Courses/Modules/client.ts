bbcab203fd40b65d75e455247e2d9a41c6867f40
jazzcort <jason101011113@gmail.com>
2024-06-09 22:49:04 -0400

    Fixed stuffs

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -18,6 +18,7 @@ export const createModule = async (courseId: string, module: any) => {
     );
     return response.data;
 };
+
 export const updateModule = async (module: any) => {
     const response = await axios.put(`${MODULES_API}/${module._id}`, module);
     return response.data;
